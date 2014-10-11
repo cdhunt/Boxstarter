3465da5b660259d96763ea7169deff8655dc323c
Chris Hunt <cdhunt@gmail.com>
2014-10-11 13:29:53 -0400

    Update DevWorkstation.ps1

--- DevWorkstation.ps1
+++ DevWorkstation.ps1
@@ -3,7 +3,7 @@ $Boxstarter.NoPassword=$false # Is this a machine with no login password?
 $Boxstarter.AutoLogin=$true # Save my password securely and auto-login after a reboot
 
 Set-ExplorerOptions -showHidenFilesFoldersDrives -showProtectedOSFiles -showFileExtensions
-Enable-RemoteDesktop
+#Enable-RemoteDesktop
 Disable-InternetExplorerESC
 
 # Posh Tools
